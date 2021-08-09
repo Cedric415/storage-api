a20a91706bc643d97a4cf216cdb510f971e662f0
Cedric415 <cedricflores415@gmail.com>
2021-08-09 09:19:16 -0700

    Añadida la funcion de añadir noticias

--- modules/inflog.py
+++ modules/inflog.py
@@ -30,9 +30,30 @@ def add_user(id = None, username = None, password = None, fecha = None, email = 
     )
     return datos_usuario
 
-def get_user(users=None):
+def get_users(users=None):
     query_result = query_storage(
         "user/users",
     )
     if users is None:
         return query_result["content"]
+
+    def add_noticia(id_noticia = None, titulo = None, noticia = None, fuente = None, fecha = None, usuario = None):
+    print("Datos de la noticia")
+    print(id_noticia, titulo, noticia, fuente, fecha, usuario)
+    print("Exito")
+
+    almacenable2 = {
+        "id_noticia": id_noticia,
+        "titulo": titulo,
+        "noticia": noticia,
+        "fuente": fuente,
+        "fecha": fecha,
+        "usuario": usuario,
+    }
+    nombre_de_archivo = f"{id_noticia}-{titulo}-{noticia}-{fuente}-{fecha}-{usuario}.json"
+    datos_noticia = store_string(
+        "noticia/noticias",
+        nombre_de_archivo,
+        json.dumps(almacenable2)
+    )
+    return datos_noticia
--- routes/inflog.py
+++ routes/inflog.py
@@ -6,7 +6,11 @@ from modules.bottles import BottleJson
 from modules.cors import enable_cors
 import modules.utils as utils
 from modules.auth import auth_required
-from modules.inflog import(add_user)
+from modules.inflog import(
+add_user,
+get_users,
+add_noticia
+)
 
 app = bottle.Bottle()
 
@@ -23,6 +27,35 @@ def store(*args, **kwargs):
         email = str(payload['email'])
         print("Datos Aceptados")
         respuesta = add_user(**payload)
+        print(respuesta)
+        print("Done")
+    except:
+        print("Datos incorrectos")
+        raise bottle.HTTPError(405, "datos invalidos")
+    raise bottle.HTTPError(201, respuesta)
+
+@app.get("/users")
+def get_all_info(*args, **kwargs):
+    try:
+       respuesta = get_users()
+    except:
+        raise bottle.HTTPError(500, "Error interno")
+    raise bottle.HTTPError(200, respuesta)
+
+## Añadir nueva noticia
+@app.post("/<id>/noticia")
+def store(*args, **kwargs):
+    payload = bottle.request.json
+    print(payload)
+    try:
+        id_noticia = str(payload['id_noticia'])
+        titulo = str(payload['titulo'])
+        noticia = str(payload['noticia'])
+        fuente = str(payload['fuente'])
+        fecha = dt.date.fromisoformat(payload['fecha'])
+        usuario = str(payload['usuario'])
+        print("Datos Aceptados")
+        respuesta = add_notica(**payload)
         print(respuesta)
         print("Done")
     except:
